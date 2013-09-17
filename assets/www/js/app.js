84fcf1f0c4d0c04af4122ef2c202e156717a62e7
Matteo Magni <matteo@magni.me>
2013-09-17 14:56:09 +0200

    web fix

--- assets/www/js/app.js
+++ assets/www/js/app.js
@@ -1,9 +1,9 @@
 define(['init'], function(init) {
 
         // for app version
-        document.addEventListener("deviceready", onSystemReady, false);
+        // document.addEventListener("deviceready", onSystemReady, false);
         //for web version
-        // $(document).ready(onSystemReady);
+        $(document).ready(onSystemReady);
         function onSystemReady() {
             init.start();
         }
