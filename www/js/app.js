d4ecb7aa28e2fccb1318159aa20f815f1d6d651f
Matteo Magni <matteo@magni.me>
2013-10-16 23:53:47 +0200

    fix cordova 3.0

--- www/js/app.js
+++ www/js/app.js
@@ -1,8 +1,11 @@
 define(['init'], function(init) {
 
-        $(document).ready(function () {
+        // for app version
+        document.addEventListener("deviceready", onSystemReady, false);
+        //for web version
+        // $(document).ready(onSystemReady);
+        function onSystemReady() {
             init.start();
-        });
-
+        }
     }
 );
