856930015eca659307a483f9d4b4cf7aae217ef6
Matteo Magni <matteo@magni.me>
2014-07-07 11:10:45 +0200

    jquery mobile  1.4.3

---
+++ assets/www/js/app.js
@@ -0,0 +1,11 @@
+define(['init'], function(init) {
+
+        // for app version
+        // document.addEventListener("deviceready", onSystemReady, false);
+        //for web version
+        $(document).ready(onSystemReady);
+        function onSystemReady() {
+            init.start();
+        }
+    }
+);
---
+++ assets/www/js/init.js
@@ -0,0 +1,37 @@
+define(['jquery', 'content'], function($, content) {
+        return {
+            start: function () {
+                //register event
+                $(document).on('click', '#panel-nav a', function () {
+                    $('#panel-nav').panel('close');
+                    window.scrollTo(0, 0);
+                });
+
+                $(document).on('click', '#sec-panel-nav a', function () {
+                    $('#sec-panel-nav').panel('close');
+                    window.scrollTo(0, 0);
+                });
+
+                $(document).on('click', '.setTitle', function () {
+                    content.setTitle($(this).data('title'));
+                });
+
+                $(document).on('click', '.setSubTitle', function () {
+                    content.setSubTitle($(this).data('title'));
+                });
+
+                $(document).on('click', '#backButton a', function (e) {
+                    e.preventDefault();
+                    history.back();
+                });
+
+                content.loadFeedTag();
+
+                content.loadPlaceTag();
+
+            }
+        }
+    }
+);
+
+
